c2819516dd710a502901dec1e5745aed4ef15b88
crangelsmith <camila.rangel.smith@gmail.com>
2020-11-19 14:34:08 +0000

    add function that applies edge detection and thresholding to lithic object (#18)

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,11 +3,13 @@ Test the functions in read_and_process.py
 """
 import pytest
 import os
-from src.read_and_process import read_image
+import yaml
+from src.read_and_process import read_image, detect_lithic
+
+import matplotlib.pyplot as plt
 
 
-
-def test_analyse_gee_data():
+def test_read_image():
 
     filename = os.path.join('tests','test_images','RDK2_17_Dc_Pc_Lc.png')
     image_array = read_image(filename)
@@ -17,3 +19,39 @@ def test_analyse_gee_data():
 
     assert image_array.shape==(1595,1465)
     assert image_array_tif.shape==(445,1548)
+
+
+def test_detect_lithic():
+
+    filename = os.path.join('tests', 'test_images', 'RDK2_17_Dc_Pc_Lc.png')
+
+    image_array = read_image(filename)
+
+    filename_config = os.path.join('tests', 'test_config.yml')
+
+    # Read YAML file
+    with open(filename_config, 'r') as config_file:
+        config_file = yaml.load(config_file)
+
+    binary_edge_sobel = detect_lithic(image_array, config_file['lithic'])
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True,
+                             figsize=(8, 8))
+    axes = axes.ravel()
+
+    axes[0].imshow(image_array, cmap=plt.cm.gray)
+    axes[0].set_title('Original image')
+
+    axes[1].imshow(binary_edge_sobel, cmap=plt.cm.gray)
+    axes[1].set_title('Sobel Edge Detection')
+
+    for ax in axes:
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(os.path.join('tests', 'edge_detection_lithic.png'))
+
+    assert binary_edge_sobel.shape==(1595,1465)
+
+
+
