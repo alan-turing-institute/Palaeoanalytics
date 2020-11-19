2a42bb60c6194285b84cfd96bfe2b12c89cc3840
crangelsmith <camila.rangel.smith@gmail.com>
2020-11-19 15:15:13 +0000

    contour finding WIP (#20)

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,7 +4,7 @@ Test the functions in read_and_process.py
 import pytest
 import os
 import yaml
-from src.read_and_process import read_image, detect_lithic
+from src.read_and_process import read_image, detect_lithic, find_lithic_contours
 
 import matplotlib.pyplot as plt
 
@@ -52,6 +52,40 @@ def test_detect_lithic():
     plt.savefig(os.path.join('tests', 'edge_detection_lithic.png'))
 
     assert binary_edge_sobel.shape==(1595,1465)
+
+def test_find_lithic_contours():
+
+    filename = os.path.join('tests', 'test_images', 'RDK2_17_Dc_Pc_Lc_A.png')
+
+    image_array = read_image(filename)
+
+    filename_config = os.path.join('tests', 'test_config.yml')
+
+    # Read YAML file
+    with open(filename_config, 'r') as config_file:
+        config_file = yaml.load(config_file)
+
+    binary_edge_sobel = detect_lithic(image_array, config_file['lithic'])
+
+    contours = find_lithic_contours(binary_edge_sobel, config_file['lithic'])
+
+    # Display the image and plot all contours found
+    fig, ax = plt.subplots()
+    ax.imshow(binary_edge_sobel, cmap=plt.cm.gray)
+
+    for contour in contours:
+        ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+
+    ax.axis('image')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
+
+    print('Numer of contours:', len(contours))
+
+    assert len(contours)!= 0
+
+
 
 
 
