9b2297b582ed24f249110e45ae37a41a2fdb349a
crangelsmith <camila.rangel.smith@gmail.com>
2020-11-20 14:14:07 +0000

    updating setup.py with the correct link (#15)

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ with open("requirements.txt", "r") as f:
 setup(
     name="pylithics",
     version="0.0.1",
-    description="Lithics and computer vision study.",
-    url="https://github.com/alan-turing-institute/monitoring-ecosystem-resilience",
+    description="A lithics study using computer vision.",
+    url="https://github.com/alan-turing-institute/Palaeoanalytics",
     author="Jason Gellis, Camila Rangel Smith",
     license="MIT",
     include_package_data=True,
     packages=["pylithics",
               "pylithics.src"],
     install_requires=REQUIRED_PACKAGES,
+    # we will need this later, i'll leave it commented as a reminder.
     #entry_points={"console_scripts": [
      #   "pyveg_calc_EC=pyveg.scripts.calc_euler_characteristic:main",
 
