222ef73fe693434b966f8fc6cbd4c5576075ee04
JasonGellis <jasonjgellis@gmail.com>
2020-11-26 15:35:55 +0000

    Implemented scale test function #10

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,7 +4,7 @@ Test the functions in read_and_process.py
 import pytest
 import os
 import yaml
-from pylithics.src.read_and_process import read_image, detect_lithic, find_lithic_contours
+from pylithics.src.read_and_process import read_image, detect_lithic, find_lithic_contours, detect_scale
 
 import matplotlib.pyplot as plt
 
@@ -86,6 +86,35 @@ def test_find_lithic_contours():
     assert len(contours)!= 0
 
 
+### Jason's first go
 
+def test_detect_scale():
+    filename = os.path.join('tests', 'test_images', 'RDK2_scale_3cm_600dpi.png')
 
+    image_array = read_image(filename)
 
+    filename_config = os.path.join('tests', 'test_config.yml')
+
+    # Read YAML file
+    with open(filename_config, 'r') as config_file:
+        config_file = yaml.load(config_file)
+
+    binary_edge_sobel = detect_scale(image_array, config_file['scale'])
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True,
+                             figsize=(8, 8))
+    axes = axes.ravel()
+
+    axes[0].imshow(image_array, cmap=plt.cm.gray)
+    axes[0].set_title('Original image')
+
+    axes[1].imshow(binary_edge_sobel, cmap=plt.cm.gray)
+    axes[1].set_title('Sobel Horizontal Edge Detection')
+
+    for ax in axes:
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(os.path.join('tests', 'edge_detection_scale.png'))
+
+    print(binary_edge_sobel.shape)
