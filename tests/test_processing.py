2924fc0df82f31efd41c3e74276ddd94580f27ce
JasonGellis <jasonjgellis@gmail.com>
2020-11-26 16:42:25 +0000

    Tested functions for scale contours #21

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,7 +4,7 @@ Test the functions in read_and_process.py
 import pytest
 import os
 import yaml
-from pylithics.src.read_and_process import read_image, detect_lithic, find_lithic_contours, detect_scale
+from pylithics.src.read_and_process import read_image, detect_lithic, find_lithic_contours, detect_scale, find_scale_contours
 
 import matplotlib.pyplot as plt
 
@@ -86,8 +86,6 @@ def test_find_lithic_contours():
     assert len(contours)!= 0
 
 
-### Jason's first go
-
 def test_detect_scale():
     filename = os.path.join('tests', 'test_images', 'RDK2_scale_3cm_600dpi.png')
 
@@ -118,3 +116,37 @@ def test_detect_scale():
     plt.savefig(os.path.join('tests', 'edge_detection_scale.png'))
 
     print(binary_edge_sobel.shape)
+
+def test_find_scale_contours():
+
+    filename = os.path.join('tests', 'test_images', 'RDK2_scale_3cm_600dpi.png')
+
+    image_array = read_image(filename)
+
+    filename_config = os.path.join('tests', 'test_config.yml')
+
+    # Read YAML file
+    with open(filename_config, 'r') as config_file:
+        config_file = yaml.load(config_file)
+
+    binary_edge_sobel = detect_scale(image_array, config_file['scale'])
+
+    contours = find_scale_contours(binary_edge_sobel, config_file['scale'])
+
+    # Display the image and plot all contours found
+    fig, ax = plt.subplots()
+    ax.imshow(binary_edge_sobel, cmap=plt.cm.gray)
+
+
+    ax.plot(contours[:, 1], contours[:, 0], linewidth=2)
+
+    ax.axis('image')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.savefig(os.path.join('tests', 'contour_detection_scale.png'))
+
+    print('Number of contours:', len(contours))
+    print('Number of pixels:', max(contours[:, 1]) - min(contours[:, 1]))
+
+    assert len(contours)!= 0
+
