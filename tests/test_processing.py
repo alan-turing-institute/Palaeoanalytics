937272285e79e22ac822d9ee26fe26dfa1ad5daf
crangelsmith <camila.rangel.smith@gmail.com>
2021-02-02 15:52:12 +0000

    fixing CI #50

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,7 +4,7 @@ Test the functions in read_and_process.py
 import pytest
 import os
 import yaml
-from pylithics.src.read_and_process import read_image, detect_lithic, find_lithic_contours, detect_scale, find_scale_contours
+from pylithics.src.read_and_process import read_image, detect_lithic, find_lithic_contours
 
 import matplotlib.pyplot as plt
 
@@ -85,68 +85,4 @@ def test_find_lithic_contours():
 
     assert len(contours)!= 0
 
-
-def test_detect_scale():
-    filename = os.path.join('tests', 'test_images', 'RDK2_scale_3cm_600dpi.png')
-
-    image_array = read_image(filename)
-
-    filename_config = os.path.join('tests', 'test_config.yml')
-
-    # Read YAML file
-    with open(filename_config, 'r') as config_file:
-        config_file = yaml.load(config_file)
-
-    binary_edge_sobel, _ = detect_scale(image_array, config_file['scale'])
-
-    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True,
-                             figsize=(8, 8))
-    axes = axes.ravel()
-
-    axes[0].imshow(image_array, cmap=plt.cm.gray)
-    axes[0].set_title('Original image')
-
-    axes[1].imshow(binary_edge_sobel, cmap=plt.cm.gray)
-    axes[1].set_title('Sobel Horizontal Edge Detection')
-
-    for ax in axes:
-        ax.axis('off')
-
-    plt.tight_layout()
-    plt.savefig(os.path.join('tests', 'edge_detection_scale.png'))
-
-    print(binary_edge_sobel.shape)
-
-def test_find_scale_contours():
-
-    filename = os.path.join('tests', 'test_images', 'RDK2_scale_3cm_600dpi.png')
-
-    image_array = read_image(filename)
-
-    filename_config = os.path.join('tests', 'test_config.yml')
-
-    # Read YAML file
-    with open(filename_config, 'r') as config_file:
-        config_file = yaml.load(config_file)
-
-    binary_edge_sobel, _ = detect_scale(image_array, config_file['scale'])
-
-    contours = find_scale_contours(binary_edge_sobel, config_file['scale'])
-
-    # Display the image and plot all contours found
-    fig, ax = plt.subplots()
-    ax.imshow(binary_edge_sobel, cmap=plt.cm.gray)
-
-
-    ax.plot(contours[:, 1], contours[:, 0], linewidth=2)
-
-    ax.axis('image')
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.savefig(os.path.join('tests', 'contour_detection_scale.png'))
-
-    print('Number of contours:', len(contours))
-    print('Number of pixels:', max(contours[:, 1]) - min(contours[:, 1]))
-
-    assert len(contours)!= 0
 
