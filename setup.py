020384c81d054d990d48595aa5bf04cb613cd362
JasonGellis <jasonjgellis@gmail.com>
2024-10-03 16:49:14 +0100

    update setup file for v1.5 and better package reading

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+# Read the requirements.txt file for package dependencies
 with open("requirements.txt", "r") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
 
 setup(
     name="PyLithics",
-    version="1.0",
+    version="1.5",
     description="A Python package for stone tool analysis",
-    download_url="https://github.com/alan-turing-institute/Palaeoanalytics",
+    download_url="https://github.com/alan-turing-institute/Palaeoanalytics/archive/refs/tags/v1.5.tar.gz",
     author="Jason Gellis, Camila Rangel Smith, Robert Foley",
     author_email='jg760@cam.ac.uk',
     license="GNU GPLv3",
-    include_package_data=True,
-    packages=["pylithics",
-              "pylithics.src",
-              "pylithics.scripts"],
-    keywords = ['lithics', 'human evolution', 'Archaeology', 'archeology',
-                'lithic analysis', 'prehistoric technology', 'computer vision'],
+    include_package_data=True,  # Ensures additional non-code files (e.g., data) are included
+    packages=find_packages(),   # Automatically find packages in the current directory
+    keywords=['lithics', 'human evolution', 'Archaeology', 'archeology',
+              'lithic analysis', 'prehistoric technology', 'computer vision'],
     install_requires=REQUIRED_PACKAGES,
-    # we will need this later, I'll leave it commented as cont reminder.
-    entry_points={"console_scripts": [
-      "pylithics_run=pylithics.scripts.run:main",
-      "pylithics_get_arrows=pylithics.scripts.run_arrows:main",
-
-    ]},
+    entry_points={
+        "console_scripts": [
+            "pylithics_run=pylithics.scripts.run:main",          # Command-line tool
+            "pylithics_get_arrows=pylithics.scripts.run_arrows:main",  # Another command-line tool
+        ],
+    },
+    long_description=open("README.md").read(),  # Optional, but recommended
+    long_description_content_type="text/markdown",  # Optional, specify format if using README.md
 )
